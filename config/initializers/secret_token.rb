664f74d0627e3cb643db00207dede7c099c18956
Junichi Ito <jit@sonicgarden.jp>
2012-09-27 11:10:21 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrackmanTest::Application.config.secret_token = 'aa0e4b4411cdc811cc58ba5d6e67fedc66e8b1baf7228c1983feecab3c59a4843bff40a4ac0c62b177ce17b8fed088db89246ef324497c5334c24908c5241f45'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TrackmanTest::Application.config.session_store :cookie_store, key: '_trackman_test_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TrackmanTest::Application.config.session_store :active_record_store
